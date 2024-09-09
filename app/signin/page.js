f24c8c3c3f750a8cb93eb620513c97cfd42336ff
Prince Aguguo <paguguo8@gmail.com>
2024-09-09 09:05:28 -0400

    Update page.js

--- app/signin/page.js
+++ app/signin/page.js
@@ -95,7 +95,7 @@ const SignIn = () => {
                         onClick={handleGoogleSignUp} 
                         className="w-full px-4 py-2 font-semibold text-white bg-red-600 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
                     >
-                        {isRegistering ? "Sign up with Google" : "Regiester with Google"} 
+                        {isRegistering ? "Register with Google" : "Sign up with Google"} 
 
                     </button>
                     
